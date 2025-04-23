df348bf507b49658927e555b26823107ea4a68f1
udemygo <eduomnipvtltd@gmail.com>
2025-04-23 14:18:20 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -72,22 +72,12 @@ export default function RootLayout({ children }) {
   });
 </script>
   
-{/* Google Tag Manager */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NDKXCVBK'); </script>
-{/* End Google Tag Manager */}
+
       </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDKXCVBK"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-{/*  End Google Tag Manager (noscript) */}
 
   
         <div className=" relative w-screen max-w-[1450px] overflow-x-hidden mx-auto lg:p-10 pt-1   ">
